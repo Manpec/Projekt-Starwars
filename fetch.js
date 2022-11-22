c3d9657adadafc52f0fab296cd459603d155bfa6
Mana Pecar <mana.pecar@gmail.com>
2022-11-22 16:31:12 +0100

    pull and fix search fetch

--- fetch.js
+++ fetch.js
@@ -1,1 +1,22 @@
-// Films and vehicles
+
+function starwarsSearch(){
+    let select =document.getElementById('select').value;
+    let search = document.getElementById('search').value;
+    let selection = select;
+    let input = search;
+    fetch(`https://swapi.dev/api/${selection}/${input}`)
+    
+    .then(response => {
+        if(response.ok) {
+       return response.json();     
+   } 
+})
+    .then(data => {
+            console.log(data) 
+         document.getElementById("result").innerHTML=`${data.name}`  
+})
+.catch(data => {
+ 
+ document.getElementById("result").innerHTML=`ERROR number does not exist, try another number`  
+})
+}
