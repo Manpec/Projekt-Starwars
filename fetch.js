d91dd4e247298d9f0c019cb8d94b59c78e1120fc
marcus therup <m_therup@hotmail.com>
2022-11-22 15:47:51 +0100

    Add fetch function

--- fetch.js
+++ fetch.js
@@ -1,1 +1,18 @@
-// Films and vehicles
+
+function starwarsSearch(){
+    let select =document.getElementById('select').value;
+    let search = document.getElementById('search').value;
+    let selection = select;
+    let input = search;
+    fetch(`https://swapi.dev/api/${selection}/${input}`)
+    
+    .then(response => {
+        if(response.ok) {
+       return response.json();     
+   } 
+})
+    .then(data => {
+            console.log(data) 
+         document.getElementById("result").innerHTML=`${data.name}`  
+})
+}
