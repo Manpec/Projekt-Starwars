6b979996917368684638ee99072b8aae76e9cfe4
Antonia Ekdahl <antoniaekdahl@gmail.com>
2022-11-22 19:31:41 +0100

    Add dropdownlist with request

--- fetch.js
+++ fetch.js
@@ -1,22 +1,41 @@
+addCategoriesToSelection();
 
-function starwarsSearch(){
-    let select =document.getElementById('select').value;
-    let search = document.getElementById('search').value;
-    let selection = select;
-    let input = search;
-    fetch(`https://swapi.dev/api/${selection}/${input}`)
-    
-    .then(response => {
-        if(response.ok) {
-       return response.json();     
-   } 
-})
-    .then(data => {
-            console.log(data) 
-         document.getElementById("result").innerHTML=`${data.name}`  
-})
-.catch(data => {
- 
- document.getElementById("result").innerHTML=`ERROR number does not exist, try another number`  
-})
+function addCategoriesToSelection() {
+  fetch("https://swapi.dev/api/")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      let arrayOfCategories = Object.keys(data); 
+      for (i = 0; i < arrayOfCategories.length; i++) {
+        let category = arrayOfCategories[i];
+        document.getElementById("select").innerHTML += `<option value="${category}">${category}</option>`;
+      }
+    });
+}
+
+function starwarsSearch() {
+  let select = document.getElementById("select").value;
+  console.log(select);
+  let search = document.getElementById("search").value;
+  console.log(search);
+  let selection = select;
+  let input = search;
+  fetch(`https://swapi.dev/api/${selection}/${input}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementById("result").innerHTML = `${data.name}`;
+    })
+    .catch((data) => {
+      document.getElementById(
+        "result"
+      ).innerHTML = `ERROR number does not exist, try another number`;
+    });
 }
