1e8cdc956523b9ccd62e816e99298e49dba487e2
Louice Rydén <louice.ryden@utb.ecutbildning.se>
2022-11-23 17:15:11 +0100

    Add error message

--- fetch.js
+++ fetch.js
@@ -23,6 +23,9 @@ function starwarsSearch() {
   console.log(select);
   let search = document.getElementById('search').value;
   console.log(search);
+  let result = document.getElementById('result');
+  let input = document.getElementById('search');
+  input.value = '';
   fetch(`https://swapi.dev/api/${select}/?search=${search}`)
     .then((response) => {
       if (response.ok) {
@@ -30,8 +33,13 @@ function starwarsSearch() {
       }
     })
     .then((data) => {
-      console.log('data', data.results);
-      renderData(data.results, select); //data.results = an array of objectresults, select = the option we chose-> people, vehicles, starships, planets, movies
+      if (data.count === 0) {
+        // If search term can't be found show this message
+        result.innerHTML = `Couldn't find what you're looking for. Please try again!`;
+      } else {
+        console.log('data', data.results);
+        renderData(data.results, select); //data.results = an array of objectresults, select = the option we chose-> people, vehicles, starships, planets, movies
+      }
     });
 }
 
