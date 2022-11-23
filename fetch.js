111aeda53a32e5924f1978cdcd3035df648faeda
marcus therup <m_therup@hotmail.com>
2022-11-23 18:47:48 +0100

    Add loading spinner

--- fetch.js
+++ fetch.js
@@ -1,11 +1,11 @@
 let api = 'https://swapi.dev/api/'
 
 
-
 addCategoriesToSelection();
 
 
 function addCategoriesToSelection() {
+  
   fetch(api)
     .then((response) => {
       if (response.ok) {
@@ -22,26 +22,34 @@ function addCategoriesToSelection() {
 }
 
 function starwarsSearch() {
+  let loader = `<div class="boxLoading"></div>`;//loader gets html
+  document.getElementById('loading').innerHTML = loader; //div id loading gets html
   let select = document.getElementById("select").value;
   console.log(select);
   let search = document.getElementById("search").value;
   console.log(search);
+  
   fetch(`${api}${select}/?search=${search}`)
     .then((response) => {
       if (response.ok) {
+        
         return response.json();
       }
     })
+
     .then((data) => {
       console.log('data',data.results);
       renderData(data.results, select) //data.results = an array of objectresults, select = the option we chose-> people, vehicles, starships, planets, movies
+      document.getElementById('loading').innerHTML = ``;//removes html from id loading
     })
+
+    
 
   }
 
   function renderData (data, option){ //data = data.results to get the array of resultobjects
    let renderResults = document.getElementById("result") //gets the div in where we are gonna render our search results
-   renderResults.innerHTML='' //Resets the innerHTML to prevent previous searches to render on the website
+   renderResults.innerHTML=``//Resets the innerHTML to prevent previous searches to render on the website
     switch (option) { //a switch which gets the parameter option which is one of [people, starships, and so on]
       case 'starships':
                 for (let i = 0; i < data.length; i++) { //for loop based on the length of the search results we get
@@ -65,9 +73,29 @@ function starwarsSearch() {
               renderResults.innerHTML += 
               `<div class='renderContent'><div class="star-wars"><div class="crawl"><p><h1>EPISODE: ${data[i].episode_id}</h1></p><p><h3>TITLE: ${data[i].title}</h3<br><br></p><p> ${data[i].opening_crawl}</p></div></div></div>`  //renders all the results
             }
-      default:
-        break;
+            default:
+                renderResults.innerHTML += 
+                `<div class='renderContent'><h1>ERROR</h1><br><br>Try a different search</div><br>`  //Text if nothing found
+              
+              break;
     }
-
+    
     
   }
+  /*
+  var loader = document.getElementById('loader');
+  var loader2 = document.getElementById('loader2');
+
+  var loaderBtn = document.getElementById('loader_Btn');
+  var loader2Btn = document.getElementById('loader2_Btn');
+
+  loaderBtn.onclick = function() {
+     loader.setAttribute('class', 'hidden');
+      loader2.setAttribute('class', 'visible');
+  };
+
+  loader2Btn.onclick = function() {
+      loader2Btn.setAttribute('class', 'hidden');
+      loaderBtn.setAttribute('class', 'visible');
+  };
+  */
