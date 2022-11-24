fc52aed13d0a7929ec4c227cbb4abff80c82f980
marcus therup <m_therup@hotmail.com>
2022-11-24 12:48:06 +0100

    Add styling

--- error.js
+++ error.js
@@ -1,7 +1,7 @@
 function error() {
     let error = document.getElementById('result') // getting the right ID
     if(error.childNodes.length === 0 ) {          // checking to see if element is empty. could also use textContent instead of childNodes to allow comments within the element                   
-      document.getElementById('result').innerHTML = `<h1>Error. Please try a different search</h1>`;    //If element is empty post error message
+      document.getElementById('result').innerHTML = `<div id="errorDiv"><h1>Error. Please try a different search</h1></div>`;    //If element is empty post error message
     }
    }
    
