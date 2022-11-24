62a5e59a0098f143c328e6c8a4ec19a7b08cb69c
marcus therup <m_therup@hotmail.com>
2022-11-24 14:07:43 +0100

    Clean up code, make more readable

--- switch.js
+++ switch.js
@@ -8,28 +8,59 @@ function renderData(data, option) {
     case 'starships':
       for (let i = 0; i < data.length; i++) {
         //for loop based on the length of the search results we get
-        renderResults.innerHTML += `<div class='renderContent'><p>Name: ${data[i].name}</p><p>Model: ${data[i].model}</p><p>Manufacturer: ${data[i].manufacturer}</p><a href="#">More</a></div>`; //renders all the results
+        renderResults.innerHTML += 
+        `<div class='renderContent'>
+            <p>Name: ${data[i].name}</p>
+            <p>Model: ${data[i].model}</p>
+            <p>Manufacturer: ${data[i].manufacturer}</p>
+            <a href="#">More</a>
+        </div>`; //renders all the results
       }
       break;
     case 'species':
       for (let i = 0; i < data.length; i++) {
-        renderResults.innerHTML += `<div class='renderContent'><p>Name: ${data[i].name}</p><p>Classification: ${data[i].classification}</p><p>Language: ${data[i].language}</p><a href="#">More</a></div>`;
+        renderResults.innerHTML += 
+        `<div class='renderContent'>
+            <p>Name: ${data[i].name}</p>
+            <p>Classification: ${data[i].classification}</p>
+            <p>Language: ${data[i].language}</p>
+            <a href="#">More</a>
+        </div>`;
       }
       break;
     case 'planets':
       for (let i = 0; i < data.length; i++) {
-        renderResults.innerHTML += `<div class='renderContent'><p>Name: ${data[i].name}</p><p>Climate: ${data[i].climate}</p><p>Population: ${data[i].population}</p><a href="#">More</a></div>`;
+        renderResults.innerHTML += 
+        `<div class='renderContent'>
+            <p>Name: ${data[i].name}</p>
+            <p>Climate: ${data[i].climate}</p>
+            <p>Population: ${data[i].population}</p>
+           <a href="#">More</a>
+        </div>`;
       }
       break;
     case 'people':
       for (let i = 0; i < data.length; i++) {
-        renderResults.innerHTML += `<div class='renderContent'><p>Name: ${data[i].name}</p><p>Birth year: ${data[i].birth_year}</p><p>Gender: ${data[i].gender}</p><a href="#">More</a></div>`;
+        renderResults.innerHTML += 
+        `<div class='renderContent'>
+            <p>Name: ${data[i].name}</p>
+            <p>Birth year: ${data[i].birth_year}</p>
+            <p>Gender: ${data[i].gender}</p>
+            <a href="#">More</a>
+        </div>`;
       }
       break;
       case 'vehicles':
         for (let i = 0; i < data.length; i++) { //for loop based on the length of the search results we get     
           renderResults.innerHTML += 
-          `<div class='renderContent'><div class="vehicles"><p>Name: ${data[i].name}</p><p>Model: ${data[i].model}</p><p>Manufacturer: ${data[i].manufacturer}</p><a href="#">More</a></div></div>`  //renders all the results           
+          `<div class='renderContent'>
+            <div class="vehicles">
+              <p>Name: ${data[i].name}</p>
+              <p>Model: ${data[i].model}</p>
+              <p>Manufacturer: ${data[i].manufacturer}</p>
+              <a href="#">More</a>
+           </div>
+          </div>`  //renders all the results           
         }
 
         break;
@@ -37,11 +68,22 @@ function renderData(data, option) {
           for (let i = 0; i < data.length; i++) { //for loop based on the length of the search results we get
             
             renderResults.innerHTML += 
-            `<div class='renderContent'><div class="star-wars"><div class="crawl"><p><h1>EPISODE: ${data[i].episode_id}</h1></p><p><h3>TITLE: ${data[i].title}</h3<br><br></p><p> ${data[i].opening_crawl}</p><a href="#">More</a></div></div></div>`  //renders all the results
+            `<div class='renderContent'>
+              <div class="star-wars">
+               <div class="crawl">
+                 <p><h1>EPISODE: ${data[i].episode_id}</h1></p>
+                 <p><h3>TITLE: ${data[i].title}</h3<br><br></p><p> ${data[i].opening_crawl}</p>
+                 <a href="#">More</a>
+                </div>
+              </div>
+            </div> `  //renders all the results
           }
           break;
     default:
-      renderResults.innerHTML += `<div class='renderContent'><p>Couldn't find what you're looking for. Please try again!</p></div>`;
+      renderResults.innerHTML += 
+      `<div class='renderContent'>
+        <p>Couldn't find what you're looking for. Please try again!</p>
+      </div>`;
       break;
   }
 }
