5d08119020ba07e60e31339bb1019794b5c5c5fc
marcus therup <m_therup@hotmail.com>
2022-11-27 11:57:09 +0100

    Changed fetch to add all pages

--- fetch.js
+++ fetch.js
@@ -1,10 +1,45 @@
 document.getElementById("searchBtn").addEventListener("click", starwarsSearch);
+document.getElementById("searchBtn").addEventListener("click", empty);  
 document.getElementById('search').addEventListener('keyup', (event) => {
   if (event.code === 'Enter') {
     starwarsSearch();
   }
 });
+function empty() {  
+  results = [];  //Empty results array
+}
+let results = [];
+async function starwarsSearch() {
+  document.getElementById('mainTitle').innerHTML = ``; //Removes animation 
+  let loader = `<div class="boxLoading"></div>`; //loader gets html
+  document.getElementById('loading').innerHTML = loader; //div id loading gets html
+  let select = document.getElementById('select').value;
+  let search = document.getElementById('search').value;
+  let result = document.getElementById('result');
+ 
+  let url = (`https://swapi.dev/api/${select}/?search=${search}`);
+  
+  do {
+    let res = await fetch(`${url}`);
+    let data = await res.json();
+    url = data.next;  // use the next url in the array
+    results.push(...data.results);  //Puts the result into the array results
+    renderData(results, select);
+    document.getElementById('loading').innerHTML = ``; //removes html from id loading when call is done
+    if (data.count === 0) {
+      // If search term can't be found show this message
+      result.innerHTML = `Couldn't find what you're looking for. Please try again!`;
+    } else {
+console.log(results)
+    //  console.log('data', data.results);
 
+    }
+  } while(url)
+  
+  return results;
+}
+
+/*OLD FETCH
 function starwarsSearch() {
   let loader = `<div class="boxLoading"></div>`;//loader gets html
   document.getElementById('loading').innerHTML = loader; //div id loading gets html
@@ -33,3 +68,4 @@ function starwarsSearch() {
       document.getElementById('mainTitle').innerHTML = ``;//removes html from id loading when call is done
     });
 }
+*/
